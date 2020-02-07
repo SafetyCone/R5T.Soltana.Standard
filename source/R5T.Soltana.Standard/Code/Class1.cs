b811bb660de94851595729a212fd878e374e5d83
MinexAutomation <minex.automation@gmail.com>
2020-02-07 00:49:22 -0800

    Added IServiceCollection extensions.

--- source/R5T.Soltana.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Soltana.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Soltana.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Hladir.CSharp.Standard;
+using R5T.Koping.Standard;
+using R5T.Lombardy.Standard;
+using R5T.Solgene;
+
+using R5T.Soltana.Default;
+
+
+namespace R5T.Soltana.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioSolutionFileOperator"/> service.
+        /// Allows specifying the <see cref="IVisualStudioSolutionFileGenerator"/>, to allow using either VS2017 or VS2019.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioSolutionFileOperator(this IServiceCollection services,
+            ServiceAction<IVisualStudioSolutionFileGenerator> addSolutionFileGenerator)
+        {
+            services
+                .AddDefaultVisualStudioSolutionFileOperator(
+                    addSolutionFileGenerator,
+                    services.AddVisualStudioSolutionFolderProjectTypeGuidProviderAction(),
+                    services.AddVisualStudioProjectFileNameConventionsAction(),
+                    services.AddStringlyTypedPathOperatorAction())
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioSolutionFileOperator"/> service.
+        /// Allows specifying the <see cref="IVisualStudioSolutionFileGenerator"/>, to allow using either VS2017 or VS2019.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileOperator> AddVisualStudioSolutionFileOperatorAction(this IServiceCollection services,
+            ServiceAction<IVisualStudioSolutionFileGenerator> addSolutionFileGenerator)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileOperator>(() => services.AddVisualStudioSolutionFileOperator(
+                addSolutionFileGenerator));
+            return serviceAction;
+        }
+    }
+}
